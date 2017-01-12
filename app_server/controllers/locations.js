578bc76cde2fcc66322ddfcc51b4a8ad7a0da4bf
dexoey <m.azarboon@gmail.com>
2017-01-12 09:05:44 +0200

    debugging info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var apiOptions = {
     server: "http://localhost:3000"
 };
 if (process.env.NODE_ENV === "production") {
-    apiOptions.server = "tranquil-island-41577.herokuapp.com";
+    apiOptions.server = "wifinder-v1-1.herokuapp.com";
 }
 var _showError = function (req, res, statusCode) {
     if (statusCode === 404) {
