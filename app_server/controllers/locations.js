579490985891f96e9a5a38f257cf9c4ec84b6453
dexoey <m.azarboon@gmail.com>
2017-01-12 09:12:24 +0200

    debugging info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,10 @@
 var request = require('request');
 var apiOptions = {
     server: "http://localhost:3000"
+    
 };
 if (process.env.NODE_ENV === "production") {
-    apiOptions.server = "wifinder-v1-1.herokuapp.com";
+    apiOptions.server = "http://wifinder-v1-1.herokuapp.com";
 }
 var _showError = function (req, res, statusCode) {
     if (statusCode === 404) {
